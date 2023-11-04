6a67c81660a7a4d1da0dff191d6b214fe7a1b4a8
Joshua Chen <97651048+jchen257@users.noreply.github.com>
2023-11-04 16:57:27 -0700

    added relative paths to image

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Pomodoro Timer',
-        imgUrl: '/assets/blank.png',
+        imgUrl: './assets/blank.png',
         stack: ['HTML', 'CSS', 'Javascript'],
         link: 'blank.com', 
     },
     {
         title: 'Developer Portfolio',
-        imgUrl: '/assets/blank.png',
+        imgUrl: './assets/blank.png',
         stack: ['React', 'Tailwind', 'Vite'],
         link: 'blank.com', 
     },
     {
         title: 'Blank Blank',
-        imgUrl: '/assets/blank.png',
+        imgUrl: './assets/blank.png',
         stack: ['HTML', 'CSS', 'Javascript'],
         link: 'blank.com', 
     },
